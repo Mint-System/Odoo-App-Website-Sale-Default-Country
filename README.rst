3af576ccc9f97fc1376dc4f45ab72f628ca9d47e
Janik von Rotz <login@janikvonrotz.ch>
2020-12-15 10:37:29 +0100

    init

---
+++ README.rst
@@ -0,0 +1,18 @@
+============================
+Website Sale Default Country
+============================
+
+.. image:: ./static/description/icon.png
+  :width: 100
+  :alt: Icon
+
+Set switzerland as the default country in the website sale address from.
+
+Usage
+~~~~~
+
+Clone module into Odoo addon directory.
+
+.. code-block:: bash
+
+    git clone git@github.com:mint-system/odoo-app-website-sale-default-country.git ./addons/website_sale_default_country
---
+++ __manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website Sale Default Country",
+
+    'summary': """
+        Set switzerland as the default country in the website sale address from.
+    """,
+
+    'description': """
+        Set switzerland as the default country in the website sale address from.
+    """,
+
+    'author': "Mint System GmbH",
+    'website': "https://www.mint-sytem.ch",
+    'category': 'Website',
+    'version': '14.0.1.0.0',
+
+    'depends': [
+        'base',
+        'website'
+    ],
+
+    'data': [
+        'views/templates.xml',
+    ],
+
+    'installable': True,
+    'application': False,
+}
